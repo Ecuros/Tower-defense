19d87ecd44fe544056e00cbde42cec2fbe103634
Ecuros2 <245580@student.pwr.edu.pl>
2019-05-25 17:15:04 +0200

    Initial commit

---
+++ Tower Defense/Assets/Scripts/HP_script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP_script : MonoBehaviour
+{
+    public int HP;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    
+    
+
+    private void damage ()
+    {
+
+        if (HP >0 && Input.GetMouseButtonDown(0))
+        {
+            HP--;
+            Debug.Log("Ouch!");
+        }
+    }
+
+}
